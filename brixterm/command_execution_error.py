60e5b0e26df406d266ad0ab10782daa2ed01fa48
Matej Kvassay <matejkvassay@icloud.com>
2025-07-26 19:02:55 +0200

    dopne

--- brixterm/command_execution_error.py
+++
@@ -1,5 +0,0 @@
-class CommandExecutionError(Exception):
-    """Raised when a shell command fails."""
-
-    def __init__(self, message="Command execution failed."):
-        super().__init__(message)
